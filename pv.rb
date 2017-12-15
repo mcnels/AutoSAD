d7da1b451ee0a38f2e723b877e862b16be025fa5
mcnels <smcnels@me.com>
2017-12-15 18:15:54 -0500

    page views distribution wrt sheet names handling progress

--- pv.rb
+++ pv.rb
@@ -34,7 +34,7 @@ while quiz_list.more? do
     quiz_list.next_page!
 end
 
-quiz_list.each do |x|
+quiz_list.each_with_index do |x, ran|
     if (x['title'].include? "Test A") || (x['title'].include? "Test B")
       next if (x['title'].include? "Bonus")
       quiz_id = x['id'].to_s
@@ -44,12 +44,13 @@ quiz_list.each do |x|
       while submissions_list.more? do
           submissions_list.next_page!
       end
-
+      puts x['title'].to_s
       submissions = Array.new(submissions_list)
-
+      sheetnames = Array.new
       submissions_list.each do |submission|
         user_id = submission['user_id'].to_s
         sheetname = ""
+        currstudent = ""
 
         # Set start time for page views to 1 hour before test start time
         start_time1 = submission['started_at'].to_s
@@ -64,104 +65,132 @@ quiz_list.each do |x|
         # Compare submission time and period_start
         startperiod = DateTime.parse(period_start)
 
-        if endt >= startperiod
-          puts startperiod
-          sheetnames = Array.new
-          # determine name for the different sheets in the excel file
-          students.each do |student|
-            if user_id == student['id'].to_s
-              sheetname = student['sortable_name']
-              # save all sheetnames in an array
+        next if endt < startperiod
+        # if endt >= startperiod
+        #sheetnames = Array.new # is a new array created every submission or does it just add to it?
+        # # determine name for the different sheets in the excel file
+        students.each do |student|
+          if user_id == student['id'].to_s
+            sheetname = student['sortable_name']
+            currstudent = student['sortable_name']
+            # save all sheetnames in an array
+            if sheetnames.include?(currstudent)
+              next
+            else
               sheetnames.push(sheetname)
             end
           end
-          puts sheetname+" started"
-
-          # submissions.each do |subm|
-          #   user_id1 = subm['user_id'].to_s
-          #   puts user_id1
-          #
-          #   students.each do |student|
-          #       puts student['id']
-          #       if user_id1 == student['id'].to_s
-          #         sheetname = student['sortable_name'].to_s
-          #         puts sheetname
-          #         # save all sheetnames in an array
-          #         sheetnames = Array.new(sheetname)
-          #       end
-          #   end
-          # end
-          #
-          #
-          # puts sheetname+" started"
-
-          # Get page views activity for each student who submitted a quiz
-          page_views = canvas.get("/api/v1/users/" + user_id + "/page_views?", {'start_time'=> start_time, 'end_time' => end_time, 'per_page' => '100'})
-
-          # Keep loading the page views till we get them all!
-          while page_views.more?  do
-              page_views.next_page!
-          end
-
-          # Check for existing sheetnames
-          sheetnames.each do |name|
-            # if name == sheetname
-            #   # Add data in existing sheet
-            #   p.workbook.add_worksheet(name: sheetname, tab_selected: true) do |sheet|
-            #     sheet.sheet_view.tab_selected = true
-            #     # create headers to specify which quiz is being reported
-            #     sheet.add_row [x['title']], :types => [:string]
-            #
-            #     # create headers
-            #     sheet.add_row ["url","controller","created_at","user_agent","participated","remote_ip","start/stop","unit test","file name","IP Switch","Browser Switch"]
-            #
-            #     page_views.each do |x|
-            #       # if (x['created_at'] == submission['started_at']) || (x['created_at'] == submission['end_at'])
-            #       #   sheet.add_row [x['url'], x['controller'], x['created_at'], x['user_agent'], x['participated'], x['remote_ip'], "Unit 1 Test A"], :types => [nil, nil, :string, :string, :string, :string, :string]
-            #       # else
-            #         sheet.add_row [x['url'], x['controller'], x['created_at'], x['user_agent'], x['participated'], x['remote_ip']], :types => [nil, nil, :string, :string, :string, :string]
-            #       # end
-            #     end
-            #
-            #     # add new line after each quiz results
-            #     sheet.add_row [""]
-            #
-            #     puts sheetname+" done"
-            #   end
-            # else
-              #Create new worksheet for student
-              p.workbook.add_worksheet do |sheet|
-                # create headers to specify which quiz is being reported
-                sheet.add_row [x['title']], :types => [:string]
-
-                # create headers
-                sheet.add_row ["url","controller","created_at","user_agent","participated","remote_ip","start/stop","unit test","file name","IP Switch","Browser Switch"]
-
-                page_views.each do |x|
-                  # if (x['created_at'] == submission['started_at']) || (x['created_at'] == submission['end_at'])
-                  #   sheet.add_row [x['url'], x['controller'], x['created_at'], x['user_agent'], x['participated'], x['remote_ip'], "Unit 1 Test A"], :types => [nil, nil, :string, :string, :string, :string, :string]
-                  # else
-                    sheet.add_row [x['url'], x['controller'], x['created_at'], x['user_agent'], x['participated'], x['remote_ip']], :types => [nil, nil, :string, :string, :string, :string]
-                  # end
-                end
-
-                # add new line after each quiz results
-                sheet.add_row [""]
-
-                # rename sheet
-                puts sheetname+" done"
-                sheet.name = sheetname
-              end
-            # end
-          end
-          # Create the Excel document
-          p.serialize('ultimate.xlsx')
-        # end
-        else
-          #next
         end
+        puts sheetnames.length
+        puts currstudent +" started"
+
+        # Get page views activity for each student who submitted a quiz
+        page_views = canvas.get("/api/v1/users/" + user_id + "/page_views?", {'start_time'=> start_time, 'end_time' => end_time, 'per_page' => '100'})
+
+        # Keep loading the page views till we get them all!
+        while page_views.more?  do
+            page_views.next_page!
+        end
+
+        # Check for existing sheetnames
+        #sheetnames.each_with_index do |name, i|
+          #puts name
+          #if (currstudent == name && i != 0) # change condition because sheetname will always be equal to name as it's just been assigned in the previous lines,
+            # need to assign the tab names differently the first time around or check for that condition only from the second iteration onwards
+
+          #if ran == 0 # handle first iteration (temp solution)
+          #   puts "no conflict"
+          #   # create headers to specify which quiz is being reported
+          #   sheet.add_row [x['title']], :types => [:string]
+          #
+          #   # create headers
+          #   sheet.add_row ["url","controller","created_at","user_agent","participated","remote_ip","start/stop","unit test","file name","IP Switch","Browser Switch"]
+          #
+          #   page_views.each do |x|
+          #     # if (x['created_at'] == submission['started_at']) || (x['created_at'] == submission['end_at'])
+          #     #   sheet.add_row [x['url'], x['controller'], x['created_at'], x['user_agent'], x['participated'], x['remote_ip'], "Unit 1 Test A"], :types => [nil, nil, :string, :string, :string, :string, :string]
+          #     # else
+          #       sheet.add_row [x['url'], x['controller'], x['created_at'], x['user_agent'], x['participated'], x['remote_ip']], :types => [nil, nil, :string, :string, :string, :string]
+          #     # end
+          #   end
+          #
+          #   # add new line after each quiz results
+          #   sheet.add_row [""]
+          #
+          #   # rename sheet
+          #
+          #   sheet.name = currstudent
+          #   puts currstudent+" done and created new sheet"
+          # else
+
+          if sheetnames.include?(currstudent) # need to skip first iteration of this
+            puts "conflict"
+            # Add data in existing sheet
+            p.workbook.add_worksheet(name: sheetname, tab_selected: true) do |sheet|
+              sheet.sheet_view.tab_selected = true
+              # create headers to specify which quiz is being reported
+              sheet.add_row [x['title']], :types => [:string]
+
+              # create headers
+              sheet.add_row ["url","controller","created_at","user_agent","participated","remote_ip","start/stop","unit test","file name","IP Switch","Browser Switch"]
+
+              page_views.each do |x|
+                # if (x['created_at'] == submission['started_at']) || (x['created_at'] == submission['end_at'])
+                #   sheet.add_row [x['url'], x['controller'], x['created_at'], x['user_agent'], x['participated'], x['remote_ip'], "Unit 1 Test A"], :types => [nil, nil, :string, :string, :string, :string, :string]
+                # else
+                  sheet.add_row [x['url'], x['controller'], x['created_at'], x['user_agent'], x['participated'], x['remote_ip']], :types => [nil, nil, :string, :string, :string, :string]
+                # end
+              end
+
+              # add new line after each quiz results
+              sheet.add_row [""]
+              # determine name for the different sheets in the excel file
+              # students.each do |student|
+              #   if curr == student['id'].to_s
+              #     sheetname = student['sortable_name']
+              #     # save all sheetnames in an array
+              #     sheetnames.push(sheetname)
+              #   end
+              # end
+              #sheet.name = name
+              puts currstudent+" done and added in existing sheet"
+            end
+          else
+            #Create new worksheet for student
+            p.workbook.add_worksheet do |sheet|
+              puts "no conflict"
+              # create headers to specify which quiz is being reported
+              sheet.add_row [x['title']], :types => [:string]
+
+              # create headers
+              sheet.add_row ["url","controller","created_at","user_agent","participated","remote_ip","start/stop","unit test","file name","IP Switch","Browser Switch"]
+
+              page_views.each do |x|
+                # if (x['created_at'] == submission['started_at']) || (x['created_at'] == submission['end_at'])
+                #   sheet.add_row [x['url'], x['controller'], x['created_at'], x['user_agent'], x['participated'], x['remote_ip'], "Unit 1 Test A"], :types => [nil, nil, :string, :string, :string, :string, :string]
+                # else
+                  sheet.add_row [x['url'], x['controller'], x['created_at'], x['user_agent'], x['participated'], x['remote_ip']], :types => [nil, nil, :string, :string, :string, :string]
+                # end
+              end
+
+              # add new line after each quiz results
+              sheet.add_row [""]
+
+              # rename sheet
+
+              sheet.name = currstudent
+              puts currstudent+" done and created new sheet"
+            end
+            puts "ready for next student"
+          end
+        #end for temp solution
+          # Create the Excel document
+          p.serialize('ultimate1.xlsx')
+        #end
       end
+      puts "ready for next test"
     else
-      #next
+      puts "not a unit test"
     end
 end
+puts "all done"
