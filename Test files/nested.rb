d336383cda51f899ce45159db727ac3135bbdf1a
mcnels sylvestre <smcnels@me.com>
2018-02-06 14:37:24 -0500

    functioning 2d array with row/column labels

--- Test files/nested.rb
+++ Test files/nested.rb
@@ -1,28 +1,23 @@
 #!/usr/bin/env ruby
-require 'canvas-api'
-require 'json'
-require 'axlsx'
-require 'date'
-
 students = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 quizzes  = ["1A", "1B", "2A", "2B", "3A", "3B", "4A", "4B", "5A", "5B"]
 
-i = 0
-dnest = Array.new(10){Array.new(10)}
+dnest = Array.new(11){Array.new(11)}
 count = 0
-
+i = 1
 students.each do |stu|
-  j = 0
+  j = 1
   quizzes.each do |q|
-    dnest[i][j] = {:stime => "start", :sub => "subtime"}
-    j = j + 1
-    count = count + 1
+      dnest[i][0] = stu
+      dnest[0][j] = q
+      dnest[i][j] = {:stime => stu, :sub => q}
+      # dnest[i][j] = {:stime => "start", :sub => "subtime"}
+      j = j + 1
+      count = count + 1
   end
   i = i + 1
 end
-
+# Print nested array
 dnest.each { |x|
   puts x.join(" ")
 }
-#puts dnest
-puts count
